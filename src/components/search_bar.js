e076b5241a783af195f2774ea71f50c25dd0e321
unknown <brcampbell001@outlook.com>
2018-04-07 23:53:11 -0500

    Section 1 Complete - Search Bar, State, Class Components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
 
-const SearchBar = () => {
-    return <input />
+        this.state = { term: ''}; //only in constructor function does this happen everywhere else this.setState is used
+    }
+    render () {
+        return (
+            // controlled component or form
+        <div>
+            <input
+            value={this.state.term}
+            onChange={event => this.setState({ term: event.target.value })} />
+        </div>
+        );
+    }
 }
 
 export default SearchBar;
