72ae87f9e6281c0e2cfe956e407dc72f9e47d60f
unknown <brcampbell001@outlook.com>
2018-04-07 23:09:42 -0500

    Basic Search Bar Component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const API_KEY = 'AIzaSyAekLdm3wFv5ejnMUhp-Q-a270CMZS0GPE';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return ( 
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
