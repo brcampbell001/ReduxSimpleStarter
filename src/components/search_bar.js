3fcf14188ae70d68c9a5048718cf27fc98f2f1d3
unknown <brcampbell001@outlook.com>
2018-04-08 10:44:23 -0500

    Vid List & Search + Styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { term: ''}; //only in constructor function does this happen everywhere else this.setState is used
-    }
-    render () {
-        return (
-            // controlled component or form
-        <div>
-            <input
-            value={this.state.term}
-            onChange={event => this.setState({ term: event.target.value })} />
-        </div>
-        );
-    }
+    this.state = { term: "" }; //only in constructor function does this happen everywhere else this.setState is used
+  }
+  render() {
+    return (
+      // controlled component or form
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
-import React from 'react'; // go find library called react and assign to variable React
-import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar';
-
-const API_KEY = 'AIzaSyAekLdm3wFv5ejnMUhp-Q-a270CMZS0GPE';
+import React, { Component } from "react"; // go find library called react and assign to variable React
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+const API_KEY = "AIzaSyAekLdm3wFv5ejnMUhp-Q-a270CMZS0GPE";
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return ( 
-    <div>
-        <SearchBar />
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("infinity war");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      }); //this can be condensed from videos:videos if key and prop are same variable name
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
     );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
 // creates an instance of app <App />    Just App would be a class component not an instance of the component
 // document.querySelector and go and find parent div container to render App in
+
+//children of App are the components | all data should be fetched in Parent level such as App
